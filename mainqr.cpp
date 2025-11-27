e4ad0296a4658ee808707762e266ef0d440b146b
CheckMate <chris.hdez.mera@gmail.com>
2025-11-27 00:48:46 -0600

    Reed Solomon
    Implement Reed-Solomon error correction and bit-to-byte conversion.

--- mainqr.cpp
+++ mainqr.cpp
@@ -72,6 +72,65 @@ vector<int>estructura_de_datos(const vector<int> &bits_iniciales){
     return bits;  
     // regreso los bits ya completos
 }
+// multiplicación en gf(2^8), aritmética modular en un día.
+int gf_mult(int a, int b) {
+    int r = 0;
+    for (int i = 0; i < 8; i++) {
+        if (b & 1) r ^= a;
+        int alto = a & 0x80;
+        a <<= 1;
+        if (alto) a ^= 0x11D;
+        b >>= 1;
+    }
+    return r & 0xFF;
+}
+
+// convierte bits a bytes debido a que reed Solomon no puede trabajar con bits, solo bytes
+vector<int> bitsABytes(const vector<int>& bits) {
+    vector<int> out;
+    for (size_t i = 0; i < bits.size(); i += 8) {
+        int x = 0;
+        for (int j = 0; j < 8; j++) {
+            x = (x << 1) | bits[i + j];
+        }
+        out.push_back(x & 0xFF);
+    }
+    return out;
+}
+
+// corrección de errores Reed-Solomon 
+vector<int> correccion_errores(const vector<int> &bitsDatos) {
+    vector<int> datos = bitsABytes(bitsDatos);
+    // como solo son 7 coeficientes, pues los pongo directamente
+    vector<int> gen = {87, 229, 146, 149, 238, 102, 21};
+    // la cantidad de bytes de corrección que se haran
+    int nsym = gen.size();
+
+    vector<int> ecc(nsym, 0);
+
+    for (size_t idx = 0; idx < datos.size(); ++idx) {
+        int d = datos[idx];
+        int f = d ^ ecc[0];
+
+        for (int i = 0; i < nsym - 1; i++)
+            ecc[i] = ecc[i + 1];
+        ecc[nsym - 1] = 0;
+        //se ultiliza el xor para saber si se va a corregiro mo
+         if (f != 0) {
+            for (int i = 0; i < nsym; i++)
+                ecc[i] ^= gf_mult(gen[i], f);
+        }
+    }
+    //transformó todo otra vez a bytes
+
+    vector<int> bitsECC;
+    bitsECC.reserve(nsym * 8);
+    for (int e : ecc) {
+        for (int b = 7; b >= 0; b--)
+            bitsECC.push_back((e >> b) & 1);
+    }
+    return bitsECC;
+}
 
 int main(){
     string url;
@@ -80,6 +139,7 @@ int main(){
 
     vector<int>datos = codificacion_de_datos(url);
     vector<int>datos_completos = estructura_de_datos(datos);
+    vector<int>bits_correccion = correccion_errores(datos_completos);
 
     return 0;
 }
